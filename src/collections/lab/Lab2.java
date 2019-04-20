648c175c2c6b78cf8a37803afb3d3a188d94cea3
Michal Niedziela <michal.niedziela@ic-consult.com>
2019-04-20 17:38:59 +0100

    Chapter 11 - Conteners - Lab 2

---
+++ src/collections/lab/Lab2.java
@@ -0,0 +1,17 @@
+package collections.lab;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class Lab2 {
+
+    public static void main(String[] args) {
+        Collection<Integer> c = new HashSet<>();
+        for(int i = 0; i < 10; i++) {
+            c.add(i);
+        }
+        for(Integer i : c) {
+            System.out.print(i + ", ");
+        }
+    }
+}
