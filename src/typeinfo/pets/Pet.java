fd57d0b3badb65e341845a6c0b9c0dd5519b79e6
michalniedziela <michal.niedziela@ic-consult.com>
2019-04-27 16:13:47 +0100

    Chapter 11 - Containers - iterator

--- src/typeinfo/pets/Pet.java
+++ src/typeinfo/pets/Pet.java
@@ -1,6 +1,9 @@
 package typeinfo.pets;
 
 public class Pet extends Individual {
+    private static int counter;
+    private final int id = counter++;
     public Pet(String name) { super(name);}
     public Pet() { super();}
+    public int id() { return id; }
 }
