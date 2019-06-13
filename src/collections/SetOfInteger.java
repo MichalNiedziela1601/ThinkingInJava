692002e93289f1922d548d4d6564207c56d8a880
Michal Niedziela <michal.niedziela@ic-consult.com>
2019-06-13 19:14:16 +0100

    Chapter 11 - Conteners - set 1

---
+++ src/collections/SetOfInteger.java
@@ -0,0 +1,16 @@
+package collections;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class SetOfInteger {
+    public static void main(String[] args) {
+        Random random = new Random(47);
+        Set<Integer> intset = new HashSet<>();
+        for(int i = 0; i < 1000; i++) {
+            intset.add(random.nextInt(30));
+        }
+        System.out.println(intset);
+    }
+}
